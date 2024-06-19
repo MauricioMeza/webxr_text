e1048d85af9aba60079327878d8a2adb8d3a2628
Mauricio Meza <mmezab@unal.edu.co>
2024-06-19 16:28:02 -0500

    default three.js VR Template

--- src/world.js
+++ src/world.js
@@ -6,6 +6,7 @@ export default class World{
     constructor(scene, renderer){
         this.loadHDRI(scene, renderer);
         this.createFloor(scene);
+        this.lighting(scene);
     }
 
     //Load HDRI image background
@@ -20,22 +21,31 @@ export default class World{
             });
     }
 
+    lighting(scene){
+        const light = new THREE.AmbientLight( 0x404040 );
+        scene.add( light );
+    }
 
     //Load floor plane
     createFloor(scene){
+        //load floor texture
+        const tex = new THREE.TextureLoader().load( "./assets/floor.jpg");
+        tex.wrapS = THREE.RepeatWrapping;
+        tex.wrapT = THREE.RepeatWrapping;
+        tex.repeat.set( 2, 2);
+
         //create bottom plane
         const planeGeometry = new THREE.PlaneGeometry(140, 140, 10, 10);
-        const planeMaterial = new THREE.MeshBasicMaterial(
-            {color:0x000000, 
+        const planeMaterial = new THREE.MeshStandardMaterial(
+            {map:tex,
             side:THREE.DoubleSide, 
-            opacity:0.75, 
+            opacity:0.8, 
             transparent:true});
         const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
         planeMesh.name = "floor";
-        const lineGeo = new THREE.WireframeGeometry( planeGeometry );
-        const lineMat = new THREE.LineBasicMaterial( { color: 0xffffff } );
-        const lineWire = new THREE.LineSegments( lineGeo, lineMat);
-        planeMesh.add( lineWire );
+
+        
+        
         
         //position bottom plane
         planeMesh.rotation.x = Math.PI / 2;
